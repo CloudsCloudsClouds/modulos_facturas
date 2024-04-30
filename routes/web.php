06ebfc21603a8b7ec1c0e4a8fed69d4bff3f91e6
Daniel Bernal <danisebber1234@gmail.com>
2024-04-30 17:41:37 -0400

    Controller Route for identity

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\IdentityController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/identity', [IdentityController::class, ''])->name('identity.index');

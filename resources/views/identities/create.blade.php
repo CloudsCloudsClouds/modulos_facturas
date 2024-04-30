bc00ce07cfd83e880c0f81596660e11114e8459d
Daniel Bernal <danisebber1234@gmail.com>
2024-04-30 17:53:25 -0400

    Forms in create for identity

---
+++ resources/views/identities/create.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=<, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Crear identidad</h1>
+    <form>
+        <div>
+            <label>First Name</label>
+            <input type="text" name="First Name" placeholder="Juan"/>
+        </div>
+        <div>
+            <label>Middle Name</label>
+            <input type="text" name="Middle Name" placeholder="Luis"/>
+        </div>
+        <div>
+            <label>Second Name</label>
+            <input type="text" name="Second Name" placeholder="Cruz"/>
+        </div>
+        <div>
+            <label>Last Name</label>
+            <input type="text" name="Last Name" placeholder="Montes"/>
+        </div>
+        <div>
+            <label>CI</label>
+            <input type="text" name="CI" placeholder="8470955"/>
+        </div>
+        <div>
+            <label>Personal Number</label>
+            <input type="text" name="Number" placeholder="77665544"/>
+        </div>
+        <div>
+            <label>Direction</label>
+            <input type="text" name="Direction" placeholder="Irpavi II Calle B"/>
+        </div>
+    </form>
+</body>
+</html>
